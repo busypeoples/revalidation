36a28a9b3ff896ae8f47eb2f26d478f7c7461bf0
busypeoples <busypeoples@gmail.com>
2017-06-11 01:27:04 +0200

    Added configurations

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  cache: true,
+  devtool: 'source-map',
+  entry: {
+    app: path.join(__dirname) + '/src/index.js'
+  },
+  output: {
+    path: path.join(__dirname) + '/public',
+    publicPath: '/',
+    filename: 'bundle.js',
+    chunkFilename: '[chunkhash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|dist)/,
+      }
+    ]
+  }
+}
