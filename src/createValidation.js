5ceeb508a109b95b9281c0bbc6fb3efebdd95564
busypeoples <busypeoples@gmail.com>
2017-06-13 13:33:51 +0200

    Add comments

--- src/createValidation.js
+++ src/createValidation.js
@@ -16,19 +16,50 @@ import {
 const { Right, Left } = Either
 
 const identity = r => r
-// create a new object containing an empty array
-// createDefaultValidation({'name': '1', 'random': '2'})
-// outputs: {name: [], random: []}
+
+/**
+ *
+ * curried function for creating a new object containing an empty array for every key
+ * createDefaultValidation({'name': '1', 'random': '2'})
+ * outputs: {name: [], random: []}
+ */
 const createDefaultValidation = map(i => [])
 
-// merge default validation rules with actual validation rules
-// createBaseValidation({name: [], random: []}, {name: [[x => x > 1, 'some msg']]})
-// outputs: {name: [[x => x > 1, 'some msg']], random: []})
+/**
+ *
+ * merge default validation rules with actual validation rules
+ * @param input
+ * @param validations
+ * @example
+ *    createBaseValidation(
+ *      {name: [], random: []},
+ *      {name: [[x => x > 1, 'some msg']]}
+ *    ) // {name: [[x => x > 1, 'some msg']], random: []})
+ */
 const createBaseValidation = (input, validations) => merge(createDefaultValidation(input), validations)
 
-const makePredicate = ([predFn, e]) => (a, all) => predFn(a, all) ? Right(a) : Left(e)
+
+/**
+ * higher order function expecting a predicate function and error message tuple.
+ * Returns a new function expecting the value and the input object.
+ * the predicate function is applied with the input and result object.
+ * if predicate function is truthy an Either.Right is rerturned else an Else.Left
+ *
+ * @param {Function} predFn the predicate function to be run
+ * @param {string} e the error message
+ */
+const makePredicate = ([predFn, e]) => (a, inputs) => predFn(a, inputs) ? Right(a) : Left(e)
+
+/**
+ * applies all predicates with the input value and inputs objects
+ *
+ * @param {*} input the value to be tested
+ * @param {Array} validations all validations to be applied with the input
+ * @param {Object} all object containing all input values
+ */
 const runPredicates = ([input, validations, all]) =>
   map(predFn => predFn(input, all), map(makePredicate, validations))
+
 
 const validator = transform => map(compose(transform, sequence(Either.of), runPredicates))
 const makeValidationObject = (input, validations) => mergeWithKey((k, l, r) => [l, r, input], input, createBaseValidation(input, validations))
