a81654869e26ebf4768347f327050983c20ac194
busypeoples <busypeoples@gmail.com>
2017-06-25 14:55:12 +0200

    Added getValue helper function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 /* @flow */
 import revalidation from './Revalidation'
+import debounce from './helpers/debounce'
+import getValue from './helpers/getValue'
 import isValid from './utils/isValid'
 
 export default revalidation
 
 export {
+  debounce,
+  getValue,
   isValid,
 }
