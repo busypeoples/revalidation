daa975d22d13de7fe1e0efb343ed30e8c478b695
busypeoples <busypeoples@gmail.com>
2017-06-27 00:26:24 +0200

    Added deep nested data example.

---
+++ example/Components/AdvancedDeepNestedData.js
@@ -0,0 +1,131 @@
+/* @flow */
+import React from 'react'
+import revalidation from '../../src/'
+import helpers from '../helpers'
+import createErrorMessage from './createErrorMessage'
+
+const {
+  isNotEmpty,
+  isLengthGreaterThan,
+  hasCapitalLetter,
+  getValue,
+} = helpers
+
+const Form = ({ revalidation: { form, updateValue, errors = {}, validateAll }, onSubmit }) =>
+  (
+    <div className='form'>
+      <div className='formGroup'>
+        <label>Name</label>
+        <input
+          type='text'
+          value={form.name}
+          onChange={e => updateValue('name', getValue(e))}
+        />
+        <div className='errorPlaceholder'>{ createErrorMessage(errors.name) }</div>
+      </div>
+      <div className='formGroup'>
+        <label>Password</label>
+        <input
+          type='password'
+          value={form.settings.password}
+          onChange={e => updateValue(['settings', 'password'], getValue(e))}
+        />
+        <div className='errorPlaceholder'>{ createErrorMessage(errors.settings.password) }</div>
+      </div>
+      <div className='formGroup'>
+        <label>Repeat Password</label>
+        <input
+          type='password'
+          value={form.settings.repeatPassword}
+          onChange={e => updateValue(['settings', 'repeatPassword'], getValue(e))}
+        />
+        <div className='errorPlaceholder'>{ createErrorMessage(errors.settings.repeatPassword) }</div>
+      </div>
+      <div className='formGroup'>
+        <label>Random</label>
+        <input
+          type='text'
+          value={form.levelOne.levelTwo.random}
+          onChange={e => updateValue(['levelOne', 'levelTwo', 'random'], getValue(e))}
+        />
+        <div className='errorPlaceholder'>{ createErrorMessage(errors.levelOne.levelTwo.random) }</div>
+      </div>
+      <button onClick={() => validateAll(onSubmit)}>Submit</button>
+    </div>
+  )
+
+const NestedForm = revalidation(Form)
+
+// validation function
+
+const isEqual = compareKey => (a, all) => a === all[compareKey]
+
+// Messages
+
+const minimumMsg = (field, len) => `Minimum ${field} length of ${len} is required.`
+const capitalLetterMag = field => `${field} should contain at least one uppercase letter.`
+const equalMsg = (field1, field2) => `${field2} should be equal with ${field1}`
+
+
+const passwordValidationRule = [
+  [isLengthGreaterThan(5), minimumMsg('Password', 6)],
+  [hasCapitalLetter, capitalLetterMag('Password')],
+]
+
+const repeatPasswordValidationRule = [
+  [isLengthGreaterThan(5), minimumMsg('RepeatedPassword', 6)],
+  [hasCapitalLetter, capitalLetterMag('RepeatedPassword')],
+  [isEqual('password'), equalMsg('Password', 'RepeatPassword')],
+]
+
+const nestedValidationRules = {
+  name: [
+    [isNotEmpty, 'Name should not be  empty.'],
+  ],
+  settings: {
+    password: passwordValidationRule,
+    repeatPassword: repeatPasswordValidationRule,
+  },
+  levelOne: {
+    levelTwo: {
+      random: [
+        [isLengthGreaterThan(7), 'Minimum Random length of 8 is required.'],
+        [hasCapitalLetter, 'Random should contain at least one uppercase letter.'],
+      ],
+    },
+  },
+}
+
+export default class AdvancedDeepNestedData extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      model: {
+        name: '',
+        settings: {
+          password: '',
+          repeatPassword: '',
+        },
+        levelOne: {
+          levelTwo: {
+            random: '',
+          }
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <NestedForm
+        onSubmit={this.props.onSubmit}
+        initialState={this.state.model}
+        rules={nestedValidationRules}
+        updateForm={this.props.updateForm}
+        validateSingle={true}
+        instantValidation={true}
+      />
+    )
+  }
+}
