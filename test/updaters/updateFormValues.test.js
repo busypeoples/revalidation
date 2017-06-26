9074ff0a4e1a89b3e338a966bb40c2ea751143ea
busypeoples <busypeoples@gmail.com>
2017-06-27 00:55:25 +0200

    Added deep nested data tests for updateForm function

--- test/updaters/updateFormValues.test.js
+++ test/updaters/updateFormValues.test.js
@@ -7,25 +7,75 @@ describe('updaters/updateFormValues', () => {
 
   it('should change the field value according to the provided value when an input has been updated', () => {
     const expected = [{form: {name: 'bar'}}, []]
-    const result = updateFormValues([{form:{name: 'foo'}}, []], UPDATE_FIELD, {name: ['name'], value: 'bar'})
+    const result = updateFormValues([{form: {name: 'foo'}}, []], UPDATE_FIELD, {name: ['name'], value: 'bar'})
     deepEqual(expected, result)
   })
 
   it('should change the field value according to the provided value when the complete form state has been updated', () => {
     const expected = [{form: {name: '', random: ''}}, []]
-    const result = updateFormValues([{form:{name: 'foo', random: 'bar'}}, []], UPDATE_ALL, {value: {name: '', random: ''}})
+    const result = updateFormValues([{form: {name: 'foo', random: 'bar'}}, []], UPDATE_ALL, {
+      value: {
+        name: '',
+        random: ''
+      }
+    })
     deepEqual(expected, result)
   })
 
   it('should not change the form state when action=VALIDATE_FIELD', () => {
     const expected = [{form: {name: 'foo', random: 'bar'}}, []]
-    const result = updateFormValues([{form:{name: 'foo', random: 'bar'}}, []], VALIDATE_FIELD, {value: {name: '', random: ''}})
+    const result = updateFormValues([{form: {name: 'foo', random: 'bar'}}, []], VALIDATE_FIELD, {
+      value: {
+        name: '',
+        random: ''
+      }
+    })
     deepEqual(expected, result)
   })
 
   it('should not change the form state when action=VALIDATE_ALL', () => {
     const expected = [{form: {name: 'foo', random: 'bar'}}, []]
-    const result = updateFormValues([{form:{name: 'foo', random: 'bar'}}, []], VALIDATE_ALL, {value: {name: '', random: ''}})
+    const result = updateFormValues([{form: {name: 'foo', random: 'bar'}}, []], VALIDATE_ALL, {
+      value: {
+        name: '',
+        random: ''
+      }
+    })
+    deepEqual(expected, result)
+  })
+
+  it('should should update a deeply nested form field when action=UPDATE_FIELD', () => {
+    const expected = [{form: {name: 'foo', levelOne: {levelTwo: {random: 'foobar'}}}}, []]
+    const result = updateFormValues([{
+      form: {
+        name: 'foo',
+        levelOne: {
+          levelTwo: {
+            random: 'bar'
+          }
+        }
+      }
+    }, []], UPDATE_FIELD, {name: ['levelOne', 'levelTwo', 'random'], value: 'foobar'})
+    deepEqual(expected, result)
+  })
+
+  it('should should update a deeply nested form when action=UPDATE_ALL', () => {
+    const expected = [{form: {name: 'foo', levelOne: {levelTwo: {random: 'bar'}}}}, []]
+    const result = updateFormValues([{
+      form: {
+        name: 'oldName',
+        levelOne: {levelTwo: {random: 'oldRandom'}}
+      }
+    }, []], UPDATE_ALL, {
+      value: {
+        name: 'foo',
+        levelOne: {
+          levelTwo: {
+            random: 'bar'
+          }
+        }
+      }
+    })
     deepEqual(expected, result)
   })
 
