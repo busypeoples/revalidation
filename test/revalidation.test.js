2787560da759bad4bb6802affdd339656e242c12
busypeoples <busypeoples@gmail.com>
2017-08-01 20:49:21 +0200

    Updated tests to work with latest changes

--- test/revalidation.test.js
+++ test/revalidation.test.js
@@ -7,8 +7,6 @@ import Revalidation, { isValid } from '../src/'
 
 const isNotEmpty = a => a.trim().length > 0
 
-const getValue = e => e.target.value
-
 const validationRules = {
   name: [[isNotEmpty, 'Name should not be empty']],
 }
@@ -17,8 +15,8 @@ const displayErrors = (errorMsgs) =>
   isValid(errorMsgs) ? null : <div className='error'>{errorMsgs[0]}</div>
 
 const Form = ({
-  revalidation: { form, validate, errors = {}, validateAll },
-  onSubmit,
+  revalidation: { form, updateValueAndValidate, errors = {}, onSubmit },
+  onSubmit: submitCb,
 }) =>
   <div className='form'>
     <div className='formGroup'>
@@ -26,11 +24,12 @@ const Form = ({
       <input
         type='text'
         value={form.name}
-        onChange={e => validate('name', getValue(e))}
+        name='name'
+        onChange={updateValueAndValidate}
       />
       {displayErrors(errors.name)}
     </div>
-    <button onClick={() => validateAll(onSubmit)}>Submit</button>
+    <button onClick={onSubmit(({ form, valid }) => valid ? submitCb(form) : null)}>Submit</button>
   </div>
 
 const initialState = { name: '' }
@@ -52,7 +51,27 @@ describe('revalidation', () => {
       />,
     )
     const output = renderer.getRenderOutput()
-    output.props.revalidation.onSubmit(output.props.onSubmit)
+    output.props.revalidation.onSubmit(({valid, form}) => valid ? output.props.onSubmit(form) : null)
     equal(wasCallbackCalled, false)
+  })
+
+  it('callback passed to `onSubmit` is called when the form is valid', () => {
+    const renderer = new ShallowRenderer()
+    const initialState = { name: 'foo' }
+    let wasCallbackCalled = false
+    renderer.render(
+      <EnhancedForm
+        initialState={initialState}
+        rules={validationRules}
+        validateSingle={false}
+        validateOnChange={false}
+        onSubmit={() => {
+          wasCallbackCalled = true
+        }}
+      />,
+    )
+    const output = renderer.getRenderOutput()
+    output.props.revalidation.onSubmit(({valid, form, errors}) => console.log('valid: ', valid, form, errors) || valid ? output.props.onSubmit(form) : null)
+    equal(wasCallbackCalled, true)
   })
 })
