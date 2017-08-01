cd665355a5fb9b6d787cdb886e3b31c52b6c9188
busypeoples <busypeoples@gmail.com>
2017-08-01 20:47:24 +0200

    Updated syncError update function to clean any field specific asyncErrors on validation

--- src/updaters/updateSyncErrors.js
+++ src/updaters/updateSyncErrors.js
@@ -31,7 +31,11 @@ export default function updateSyncErrors (state: Object, type: Array<string>, en
   const updateState = cond([
     [
       type => contains(VALIDATE_FIELD, type) && name,
-      always(assocPath(['errors', ...name], path([...name], errors), state)),
+      () => {
+        // reset any asyncErrors as the field value has been updated
+        const cleanedState = path(['asyncErrors', name], state) ? assocPath(['asyncErrors', name], [], state) : state
+        return assocPath(['errors', ...name], path([...name], errors), cleanedState)
+      },
     ],
     [
       contains(VALIDATE_ALL),
